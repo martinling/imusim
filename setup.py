e602ea69feb8afc39319b1658a881081a25796e8
Max Lapan <max.lapan@gmail.com>
2019-04-03 11:27:39 +0300

    Building issues

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with IMUSim.  If not, see <http://www.gnu.org/licenses/>.
+import os
 
 depsOK = True
 
@@ -23,23 +24,33 @@ try:
     import numpy
 except ImportError:
     depsOK = False
-    print "NumPy should be installed first from suitable binaries."
-    print "See http://numpy.scipy.org/"
+    print("NumPy should be installed first from suitable binaries.")
+    print("See http://numpy.scipy.org/")
 
 try:
     import scipy
 except ImportError:
     depsOK = False
-    print "SciPy should be installed first from suitable binaries."
-    print "See http://www.scipy.org/"
+    print("SciPy should be installed first from suitable binaries.")
+    print("See http://www.scipy.org/")
 
 try:
     import matplotlib
 except ImportError:
     depsOK = False
-    print "Matplotlib should be installed first from suitable binaries."
-    print "See http://matplotlib.sf.net/"
+    print("Matplotlib should be installed first from suitable binaries.")
+    print("See http://matplotlib.sf.net/")
 
+try:
+    import cython
+
+    if not all(map(os.path.exists, ['imusim/maths/quaternions.c', 'imusim/maths/natural_neighbour.c',
+                                    'imusim/maths/quat_splines.c', 'imusim/maths/vectors.c'])):
+        depsOK = False
+        print("You need to generate c modules by running 'cython -a imusim/maths/*.pyx'")
+except ImportError:
+    depsOK = False
+    print("Cython need to be installed, we need it to generate quaternions lib")
 try:
     import mayavi
 except ImportError:
@@ -47,8 +58,8 @@ except ImportError:
         import enthought.mayavi
     except ImportError:
         depsOK = False
-        print "Mayavi should be installed first from suitable binaries."
-        print "See http://code.enthought.com/projects/mayavi/"
+        print("Mayavi should be installed first from suitable binaries.")
+        print("See http://code.enthought.com/projects/mayavi/")
 
 try:
     from setuptools import setup, find_packages
@@ -76,4 +87,4 @@ try:
                     'imusim/maths/natural_neighbour.c'])]
         )
 except ImportError:
-    print "Setuptools must be installed - see http://pypi.python.org/pypi/setuptools"
+    print("Setuptools must be installed - see http://pypi.python.org/pypi/setuptools")
